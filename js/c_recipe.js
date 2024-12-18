d831db41e60cbb6de596fa5fe37b65ca9076e468
JiKl Coding <klatovskyjiri@gmail.com>
2024-12-18 09:16:57 +0100

    class

---
+++ js/c_recipe.js
@@ -0,0 +1,11 @@
+export class recipe {
+    constructor(id,name, type, image, portions, ingredients, desc) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.image = image;
+        this.portions = portions;
+        this.ingredients = ingredients;
+        this.desc = desc;
+    }
+}
